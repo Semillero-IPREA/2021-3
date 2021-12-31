f7ca20f428a30624cb33d0a96186f64bee1c14d8
Luis Alejandro Másmela Caita <lamasmelac@gmail.com>
2021-12-30 20:23:51 -0500

    Se incluye caja para descripción de la simulación y descripción del problema que se simula.

--- script_4.R
+++ script_4.R
@@ -1,6 +1,20 @@
-##DISTRIBUCION MUESTRAL DE LA MEDIA
+#####################################
+##DISTRIBUCION MUESTRAL DE LA MEDIA #
+#####################################
 
-#EJEMPLO 8.7
+#EJEMPLO 8.7 Walpole et.al (2007)
+#Un importante proceso de fabricación produce partes de componentes 
+#cilíndricos para la industria automotriz. Es importante que el 
+#proceso produzca partes que tengan una media de 5 milímetros. 
+#El ingeniero implicado hace la conjetura de que la media de la 
+#población es de 5.0 milímetros. Se lleva a cabo un experimento 
+#donde se seleccionan al azar 100 partes elaboradas por el proceso 
+#y se mide el diámetro de cada una de ellas. Se sabe que la desviación 
+#estándar de la población es sigma = 0.1. El experimento indica un diámetro 
+#promedio de la muestra x_barra = 5.027 milímetros. ¿Esta información de la muestra 
+#parece apoyar o refutar la conjetura del ingeniero?
+
+  
 #repeticiones del experimento
 rep = 10000
 #tamano de la muestra
